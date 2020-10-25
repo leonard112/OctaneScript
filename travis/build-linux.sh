24d0cec8fa8f72292884074dbd8f06490dda44ec
Leonard James Carcaramo Jr <lcarcaramo@gmail.com>
2020-10-25 15:46:28 -0400

    Upload README to sourceforge

--- travis/build-linux.sh
+++ travis/build-linux.sh
@@ -18,3 +18,4 @@ chmod 400 /tmp/sftp_rsa
 cp $LICENSE .
 tar -czvf $FILE_NAME_LINUX *
 sftp -o "StrictHostKeyChecking=no" -i /tmp/sftp_rsa lcarcaramo@frs.sourceforge.net:/home/frs/project/octane-lang/alpha/linux <<< $"put ${FILE_NAME_LINUX}"
+sftp -o "StrictHostKeyChecking=no" -i /tmp/sftp_rsa lcarcaramo@frs.sourceforge.net:/home/frs/project/octane-lang <<< $"put ${README}"
