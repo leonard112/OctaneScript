02d29ee4cd12375dc33bfb3323d3fed920a13665
Leonard James Carcaramo Jr <lcarcaramo@gmail.com>
2020-11-22 18:09:46 -0500

    debug

--- travis/build-linux.sh
+++ travis/build-linux.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$RELEASE_STATUS" == "DEV" ]; then
-    export PACKAGE_NAME="$NAME_LOWER-$VERSION:$COMMIT_SHORT-$RELEASE_STATUS"
+    export PACKAGE_NAME="$NAME_LOWER-$VERSION($COMMIT_SHORT)-$RELEASE_STATUS"
 else
     export PACKAGE_NAME="$NAME_LOWER-$VERSION"
 fi
@@ -33,7 +33,7 @@ echo -e "SMOKE TEST:" && cd dist && ./octane --version
 echo -e "${SFTP_KEY}" > /tmp/sftp_rsa
 chmod 400 /tmp/sftp_rsa
 cp $LICENSE .
-tar -czvf package.tgz * && mv package.tgz $PACKAGE_NAME.tgz
+tar -czvf $PACKAGE_NAME.tgz *
 sftp -o "StrictHostKeyChecking=no" -i /tmp/sftp_rsa lcarcaramo@frs.sourceforge.net:/home/frs/project/octane-lang/$RELEASE_LOWER/$OS/$ARCH/$RELEASE_STATUS_LOWER/tar <<< $"put ${PACKAGE_NAME}.tgz"
 
 # Package For Debian
