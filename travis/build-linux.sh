0386ed2b63b1b53311557ea3435f3708c2c460d6
Leonard James Carcaramo Jr <lcarcaramo@gmail.com>
2020-11-22 17:59:30 -0500

    debug

--- travis/build-linux.sh
+++ travis/build-linux.sh
@@ -37,6 +37,7 @@ tar -czvf package.tgz * && mv package.tgz $PACKAGE_NAME.tgz
 sftp -o "StrictHostKeyChecking=no" -i /tmp/sftp_rsa lcarcaramo@frs.sourceforge.net:/home/frs/project/octane-lang/$RELEASE_LOWER/$OS/$ARCH/$RELEASE_STATUS_LOWER/tar <<< $"put ${PACKAGE_NAME}.tgz"
 
 # Package For Debian
+ls
 cp octane $DEB_PACKAGE_DIR/package/usr/bin
 cd $DEB_PACKAGE_DIR && dpkg --build $PACKAGE_NAME
 sftp -o "StrictHostKeyChecking=no" -i /tmp/sftp_rsa lcarcaramo@frs.sourceforge.net:/home/frs/project/octane-lang/$RELEASE_LOWER/$OS/$ARCH/$RELEASE_STATUS_LOWER/debian <<< $"put ${PACKAGE_NAME}.deb"
